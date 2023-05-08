ddf810bbdc0d39ce07cfb7ae44358a2159b53128
ethanc-ec <44012676+ethanc-ec@users.noreply.github.com>
2023-05-08 15:39:18 -0400

    Created jitter module

---
+++ otter/assign/jitter.py
@@ -0,0 +1,63 @@
+"""Jitter for questions in Otter Assign"""
+
+import re
+from numpy.random import choice
+
+
+class Jitter:
+    """"""
+    def __init__(self, notebook):
+        self.notebook = notebook
+        self.values = []
+        self.counter = 0
+
+
+    def get_value(self) -> str:
+        """
+        Args:
+            None - called on a ``Jitter`` object
+        Returns:
+            ``str``: the next value in ``self.values``
+        """
+        value = self.values[self.counter]
+        self.counter += 1
+        return str(value)
+
+
+    def value_from_range(self, matches) -> None:
+        """ 
+        Args:
+            matches (``re.Match callable_iterator``): a list of matches from a regex search
+        Returns:
+            None - sets ``self.values`` to a list of values from the ranges in ``matches``   
+        """
+
+        # becomes [['int', 'int', 'int'], ...]
+        str_range = [re.sub(r'<<|>>', '', x.group()).split(',') for x in matches]
+
+        # becomes [[int, int, int], ...]
+        int_range = [[int(x) for x in cleaned_match] for cleaned_match in str_range]
+
+        # choose a random value from each range
+        self.values = [choice(range(*set_of_values)) for set_of_values in int_range]
+
+
+    def jitter(self):
+        """
+        Args:
+            None - called on a ``Jitter`` object
+
+        Returns:
+            ``nbformat.NotebookNode``: the jitter transformed notebook
+        """
+        for i, cell in enumerate(self.notebook['cells']):
+            if cell['cell_type'] in ['raw', 'markdown'] and bool(re.search(r'<<.+?>>', cell['source'])):
+
+                all_match = re.finditer(r'<<\d+?,([\s])?\d+?([\,]([\s])?\d+?)?>>', cell['source'])
+
+                self.value_from_range(all_match)
+
+                while bool(re.search(r'<<.+?>>', cell['source'])):
+                    self.notebook['cells'][i]['source'] = re.sub(r'<<.+?>>', self.get_value(), cell['source'], 1)
+
+        return self.notebook
