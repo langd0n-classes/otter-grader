2e9ae27c61745886ec0223d42c5045302443b221
ethanc-ec <44012676+ethanc-ec@users.noreply.github.com>
2023-05-26 12:35:32 -0700

    Jitter completed [no docs]

--- otter/assign/jitter.py
+++ otter/assign/jitter.py
@@ -1,71 +1,58 @@
 """Jitter for questions in Otter Assign"""
 
 import re
+
+from nbformat.notebooknode import NotebookNode
+from numpy import arange
 from numpy.random import choice
 
 
 class Jitter:
-    """
-    A class for jittering a notebook.
-    
-    Searches for all instances of ``<<int, int[, int]>>`` in markdown cells and 
-    replaces them with a random value as specified by the range.
-    
-    Args:
-        notebook (``nbformat.NotebookNode``): the notebook to be jittered
-    """
-    def __init__(self, notebook):
-        self.notebook = notebook
-        self.values = []
-        self.counter = 0
+    def __init__(self, nb):
+        self.nb = nb
+        self.values = {}
 
 
-    def get_value(self) -> str:
+    def value_from_range(self, matches) -> list:
+        """ Takes an iterator of match objects and returns a list of lists of integers for range()
         """
-        Args:
-            None - called on a ``Jitter`` object
-        Returns:
-            ``str``: the next value in ``self.values``
-        """
-        value = self.values[self.counter]
-        self.counter += 1
-        return str(value)
-
-
-    def value_from_range(self, matches) -> None:
-        """ 
-        Args:
-            matches (``re.Match callable_iterator``): a list of matches from a regex search
-        Returns:
-            None - sets ``self.values`` to a list of values from the ranges in ``matches``   
-        """
-
-        # becomes [['int', 'int', 'int'], ...]
+        
+        # becomes [['num', 'num', 'num'], ...]
         str_range = [re.sub(r'<<|>>', '', x.group()).split(',') for x in matches]
 
-        # becomes [[int, int, int], ...]
-        int_range = [[int(x) for x in cleaned_match] for cleaned_match in str_range]
+        # becomes [[float, float, float], ...]
+        input_range = [[float(x) for x in cleaned_match] for cleaned_match in str_range]
 
         # choose a random value from each range
-        self.values = [choice(range(*set_of_values)) for set_of_values in int_range]
+        random_nums = [choice(arange(*set_of_values)) for set_of_values in input_range] # using numpy to allow for floats
+
+        for idx, val in enumerate(random_nums):
+            if int(val) == val:
+                random_nums[idx] = int(val)
+
+        return random_nums
 
 
-    def jitter(self):
-        """
-        Args:
-            None - called on a ``Jitter`` object
+    def jitter(self) -> NotebookNode:
+        for i, cell in enumerate(self.nb['cells']):
+            regex = r'<<\d+?,([\s])?\d+?([\,]([\s])?\d+?(.\d+?)?)?>>'
 
-        Returns:
-            ``nbformat.NotebookNode``: the jitter transformed notebook
-        """
-        for i, cell in enumerate(self.notebook['cells']):
-            if cell['cell_type'] in ['raw', 'markdown'] and bool(re.search(r'<<.+?>>', cell['source'])):
+            if bool(re.search(regex, cell['source'])):
+                all_match = re.finditer(regex, cell['source'])
+                local_values = self.value_from_range(all_match)
 
-                all_match = re.finditer(r'<<\d+?,([\s])?\d+?([\,]([\s])?\d+?)?>>', cell['source'])
+                while local_values:
+                    self.nb['cells'][i]['source'] = re.sub(regex, f'<<{len(self.values)}>>', cell['source'], 1)
+                    self.values[len(self.values)] = local_values.pop(0)
 
-                self.value_from_range(all_match)
+        for i, cell in enumerate(self.nb['cells']):
+            if bool(re.search(r'<<\d+?>>', cell['source'])):
+                all_match = re.finditer(r'<<\d+?>>', cell['source'])
 
-                while bool(re.search(r'<<.+?>>', cell['source'])):
-                    self.notebook['cells'][i]['source'] = re.sub(r'<<.+?>>', self.get_value(), cell['source'], 1)
+                for match in all_match:
+                    self.nb['cells'][i]['source'] = re.sub(r'<<\d+?>>', str(self.values[int(match.group()[2:-2])]), cell['source'], 1)
 
-        return self.notebook
+        print(self.values)
+
+        return self.nb
+  
