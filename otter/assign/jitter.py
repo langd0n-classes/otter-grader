c72fd72f93ba824db06bed93c9dd024f5008ed7d
ethanc-ec <44012676+ethanc-ec@users.noreply.github.com>
2023-06-02 12:02:45 -0700

    Working jitter, swapped to (()) instead of <<>>

--- otter/assign/jitter.py
+++ otter/assign/jitter.py
@@ -7,11 +7,15 @@ from numpy import arange
 from numpy.random import choice
 
 
+
 class Jitter:
     """Jitter for questions in Otter Assign"""
     def __init__(self, notebook):
         self.notebook = notebook
+        self.has_jitter = False
         self.values = {}
+        self.question_name = ''
+        self.question_values = []
 
 
     def value_from_range(self, matches) -> list:
@@ -19,7 +23,7 @@ class Jitter:
         """
 
         # becomes [['num', 'num', 'num'], ...]
-        str_range = [re.sub(r'<<|>>', '', x.group()).split(',') for x in matches]
+        str_range = [re.sub(r'\(\(|\)\)', '', x.group()).split(',') for x in matches]
 
         # becomes [[float, float, float], ...]
         input_range = [[float(x) for x in cleaned_match] for cleaned_match in str_range]
@@ -37,28 +41,48 @@ class Jitter:
     def jitter(self) -> NotebookNode:
         """Jitters the notebook"""
 
-        # find all ranges and replace with <<num>>
+        regex = r'\(\(\d+?,([\s])?\d+?([\,]([\s])?\d+?(.\d+?)?)?\)\)'
+        short_regex = r'\(\(\d+?\)\)'
+
         for i, cell in enumerate(self.notebook['cells']):
-            regex = r'<<\d+?,([\s])?\d+?([\,]([\s])?\d+?(.\d+?)?)?>>'
+            if '# BEGIN QUESTION' in cell['source'].upper():
+                self.question_name = [line.split(':')[1] for line in cell['source'].split('\n') \
+                    if 'name' in line.lower()][0]
+
+                if 'jitter' in cell['source'].lower():
+                    self.has_jitter = [line.split(':')[1] for line in cell['source'].split('\n') \
+                        if 'jitter' in line.lower()][0].strip().lower() == 'true'
+
+                self.values[self.question_name] = []
+
+            elif '# END QUESTION' in cell['source'].upper() or not self.has_jitter:
+                # reset to default values when current question ends
+                self.question_name = ''
+                self.has_jitter = False
+                continue
 
             if bool(re.search(regex, cell['source'])):
                 all_match = re.finditer(regex, cell['source'])
                 local_values = self.value_from_range(all_match)
+                full_len = len(local_values)
+
+                self.values[self.question_name].extend(local_values)
 
                 while local_values:
                     self.notebook['cells'][i]['source'] = re.sub(regex, \
-                        f'<<{len(self.values)}>>', cell['source'], 1)
+                        f'(({full_len - len(local_values)}))', cell['source'], 1)
 
-                    self.values[len(self.values)] = local_values.pop(0)
+                    local_values.pop(0)
 
-        # replace all <<num>> with values
-        for i, cell in enumerate(self.notebook['cells']):
-            if bool(re.search(r'<<\d+?>>', cell['source'])):
-                all_match = re.finditer(r'<<\d+?>>', cell['source'])
+            if bool(re.search(short_regex, cell['source'])):
+                while bool(re.search(short_regex, cell['source'])):
+                    all_match = re.finditer(short_regex, cell['source'])
 
-                for match in all_match:
-                    self.notebook['cells'][i]['source'] = re.sub(r'<<\d+?>>', \
-                        str(self.values[int(match.group()[2:-2])]), cell['source'], 1)
+                    for match in all_match:
+                        self.notebook['cells'][i]['source'] = re.sub(short_regex, \
+                            str(self.values[self.question_name][int(match.group()[2:-2])]), \
+                                cell['source'], 1)
+
 
         return self.notebook
   
